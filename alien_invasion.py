1b25374ba0587be54d628e24e61027306442e6e8
Rudy Bakker <rudy.bakkerjr@gmail.com>
2019-12-24 12:43:11 -0500

    Setup a Settings class file to store global game settings. Setup a Ship class file to store Ship settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,14 +1,19 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         """ Initialize the game and create the game resources"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        # Setup the game window
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
@@ -21,6 +26,9 @@ class AlienInvasion:
 
             # Make the most recently drawn screen visible
             pygame.display.flip()
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
 
 if __name__ == '__main__':
     # Make a game instance and then run the game
